03384ea90f5553e0cf4cd3d938d001c21db84306
Tomas <tomaslonnblad@gmail.com>
2025-12-07 09:29:20 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
         'firefly-radial': "radial-gradient(50% 50% at 50% 50%, rgba(253, 255, 80, 0.5) 0%, rgba(217,217,217, 0) 100%)"
       },
       boxShadow:{
-        'glass-inset': 'inset 0 17px 5px -9px rgba(254,254,91, 0.05)',
+        'glass-inset': 'inset 0 17px 5px -9px rgba(7, 7, 217, 1.0)',
         'glass-sm': '5px 5px 20px 0px rgba(254,254,91, 0.3)',
       },
       keyframes:{
