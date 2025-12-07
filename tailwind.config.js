934155f85f188e3ccde82ddf85263c863c7fc7e5
Tomas <tomaslonnblad@gmail.com>
2025-12-07 09:42:43 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,10 +17,10 @@ module.exports = {
         accent: 'rgb(var(--accent))',
       },
       backgroundImage:{
-        'firefly-radial': "radial-gradient(50% 50% at 50% 50%, rgba(217,217,217, 0) 0%, rgba(253, 255, 80, 0.5) 100%)"
+        'firefly-radial': "radial-gradient(50% 50% at 50% 50%, rgba(7, 7, 217, 1.0) 0%, rgba(217,217,217, 0) 100%)"
       },
       boxShadow:{
-        'glass-inset': 'inset 0 17px 5px -9px rgba(7, 7, 217, 1.0)',
+        'glass-inset': 'inset 0 17px 5px -9px rgba(254,254,91, 0.05)',
         'glass-sm': '5px 5px 20px 0px rgba(254,254,91, 0.3)',
       },
       keyframes:{
